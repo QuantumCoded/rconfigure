1cb02e9411c46c2c52035a898b6e38fb28c97294
QuantumCoded <bfields3@jscc.edu>
2022-01-07 14:14:12 -0600

    add helper function to find files with unspecified toml extension

--- src/path.rs
+++ src/path.rs
@@ -7,3 +7,19 @@ pub fn force_absolute<P: AsRef<Path>>(path: P, pre: P) -> PathBuf {
         pre.as_ref().join(path)
     }
 }
+
+pub fn find_config_file<P: AsRef<Path>>(path: P) -> Option<PathBuf> {
+    let path = path.as_ref();
+
+    if path.exists() {
+        Some(path.to_owned())
+    } else {
+        let path = path.with_extension("toml");
+
+        if path.exists() {
+            Some(path)
+        } else {
+            None
+        }
+    }
+}
