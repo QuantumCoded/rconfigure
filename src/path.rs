433ac42ac6598ab8c70bafb40b990d434d736469
QuantumCoded <bfields3@jscc.edu>
2022-01-10 12:23:30 -0600

    fix resolving paths when appending extensions

--- src/path.rs
+++ src/path.rs
@@ -5,8 +5,11 @@ use thiserror::Error;
 /// The error type for interacting with config directory paths.
 #[derive(Error, Debug)]
 pub enum Error {
+    #[error("there is no file named {name:?} in {path:?}")]
+    FileNotFound { name: OsString, path: PathBuf },
+
     #[error("there is no file named {name:?} or {name_toml:?} in {path:?}")]
-    FileNotFound {
+    FilesNotFound {
         name: OsString,
         name_toml: OsString,
         path: PathBuf,
@@ -17,26 +20,80 @@ pub enum Error {
 }
 
 /// Resolves a user provided `path` to an absolute `PathBuf` using the absolute prefix `pre` if
-/// necessary. \
+/// necessary. Changes
+///
+/// Checks if `path` is absolute, if it's not, prepends the path `pre` \
 /// Note: `pre` must be absolute.
+///
+/// Looks for the path specified, if it doesn't exist, checks the same path with `.toml` extension. \
+/// Gives back the path that exists.
+///
+/// # Errors
+///
+/// ## FileNotFound
+/// Happens when `path` has `.toml` extension and does not exist.
+///
+/// ## FilesNotFound
+/// Happens when `path` does not have `.toml` extension and neither it nor it with `.toml`
+/// **appended** to the file name (not with the current extension changed).
 pub fn resolve(path: impl AsRef<Path>, pre: impl AsRef<Path>) -> Result<PathBuf, Error> {
     let path = force_absolute(path.as_ref().to_owned(), pre);
-    let path = find_config_file(&path).ok_or(Error::FileNotFound {
-        name: path
-            .file_name()
-            .ok_or(Error::RootOrPrefix(path.clone()))?
-            .to_owned(),
+    let path = find_config_file(&path).ok_or(match path.extension() {
+        Some(s) if s == OsString::from("toml") => Error::FileNotFound {
+            name: path
+                .file_name()
+                .ok_or(Error::RootOrPrefix(path.clone()))?
+                .to_owned(),
 
-        name_toml: path
-            .with_extension("toml")
-            .file_name()
-            .ok_or(Error::RootOrPrefix(path.clone()))?
-            .to_owned(),
+            path: path
+                .parent()
+                .ok_or(Error::RootOrPrefix(path.clone()))?
+                .to_owned(),
+        },
 
-        path: path
-            .parent()
-            .ok_or(Error::RootOrPrefix(path.clone()))?
-            .to_owned(),
+        Some(s) => {
+            let mut str = OsString::new();
+            str.push(s);
+            str.push(".toml");
+
+            Error::FilesNotFound {
+                name: path
+                    .file_name()
+                    .ok_or(Error::RootOrPrefix(path.clone()))?
+                    .to_owned(),
+
+                name_toml: path
+                    .with_extension(str)
+                    .file_name()
+                    .ok_or(Error::RootOrPrefix(path.clone()))?
+                    .to_owned(),
+
+                path: path
+                    .parent()
+                    .ok_or(Error::RootOrPrefix(path.clone()))?
+                    .to_owned(),
+            }
+        }
+
+        None => {
+            Error::FilesNotFound {
+                name: path
+                    .file_name()
+                    .ok_or(Error::RootOrPrefix(path.clone()))?
+                    .to_owned(),
+
+                name_toml: path
+                    .with_extension("toml")
+                    .file_name()
+                    .ok_or(Error::RootOrPrefix(path.clone()))?
+                    .to_owned(),
+
+                path: path
+                    .parent()
+                    .ok_or(Error::RootOrPrefix(path.clone()))?
+                    .to_owned(),
+            }
+        }
     })?;
 
     Ok(path)
@@ -44,7 +101,7 @@ pub fn resolve(path: impl AsRef<Path>, pre: impl AsRef<Path>) -> Result<PathBuf,
 
 /// Checks if `path` is absolute, if it's not, prepends the path `pre` and returns. \
 /// Note: `pre` must be absolute.
-fn force_absolute(path: impl AsRef<Path>, pre: impl AsRef<Path>) -> PathBuf {
+pub fn force_absolute(path: impl AsRef<Path>, pre: impl AsRef<Path>) -> PathBuf {
     assert!(pre.as_ref().is_absolute(), "the pre path must be absolute");
 
     if path.as_ref().is_absolute() {
@@ -54,7 +111,9 @@ fn force_absolute(path: impl AsRef<Path>, pre: impl AsRef<Path>) -> PathBuf {
     }
 }
 
-/// Looks for the path specified, if it doesn't exist, checks the same path with `.toml` extension.
+/// Looks for the path specified, if it doesn't exist, checks the same path with `.toml` extension
+/// appened (not with the current extension changed).
+///
 /// If either exist, returns `Some(PathBuf)` for the first found, otherwise returns `None`.
 fn find_config_file<P: AsRef<Path>>(path: P) -> Option<PathBuf> {
     let path = path.as_ref();
@@ -62,7 +121,17 @@ fn find_config_file<P: AsRef<Path>>(path: P) -> Option<PathBuf> {
     if path.exists() {
         Some(path.to_owned())
     } else {
-        let path = path.with_extension("toml");
+        let path = match path.extension() {
+            Some(s) => {
+                let mut str = OsString::new();
+                str.push(s);
+                str.push(".toml");
+
+                path.with_extension(str)
+            }
+
+            None => path.with_extension("toml"),
+        };
 
         if path.exists() {
             Some(path)
