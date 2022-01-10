b9acc2fed89442ddf4738eaec9971e3bcc111ca6
QuantumCoded <bfields3@jscc.edu>
2022-01-10 06:10:46 -0600

    move values to their own module

---
+++ src/value.rs
@@ -0,0 +1,24 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+/// The values in settings and profiles, used for templating
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(untagged)]
+pub enum Value {
+    Boolean(bool),
+    Integer(i64),
+    Float(f64),
+    String(String),
+    Script { script: String, value: Box<ScriptValue> },
+    ProfileValue { #[serde(rename = "use")] using: String },
+    MultiLineString(Vec<String>),
+}
+
+/// The values passed into rhai scripts, used for processing
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(untagged)]
+pub enum ScriptValue {
+    Array(Vec<ScriptValue>),
+    Value(Value),
+    Map(HashMap<String, ScriptValue>),
+}
